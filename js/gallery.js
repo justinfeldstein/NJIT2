f726c72b6909411b8e0bd77aa02991315b0fe377
Justin Feldstein <92735599+justinfeldstein@users.noreply.github.com>
2023-02-03 13:02:45 -0500

    Milestone 1 complete
    added variables and JSON constructor

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,15 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-  
+window.requestAnimFrame = (function () {
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (/* function */ callback, /* DOMElement */ element) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -18,7 +18,7 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
+	requestAnimFrame(animate);
 	var currentTime = new Date().getTime();
 	if (mLastFrameTime === 0) {
 		mLastFrameTime = currentTime;
@@ -34,10 +34,17 @@ function animate() {
 
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
+	
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
 	console.log('swap photo');
+
+	document.getElementById('photo').src = mImages[mCurrentIndex].img;
+	var location = document.getElementsByClassName("location");
+	location[0].innerHTML = 
+	var description = document.getElementsByClassName("description");
+	var data = document.getElementsByClassName("date");
 }
 
 // Counter for the mImages array
@@ -57,37 +64,55 @@ var mJson;
 var mUrl = images.json;
 
 
+//constructor for pulling JSON File
+function WhosJason() {
+	mRequest.onreadystatechange = function () {
+		console.log(object);
+		if (this.readyState == 4 && this.status == 200) {
+			mJson = JSON.parse(mRequest.responseText);
+			iterateJSON(mJSON);
+		}
+	}
+	mRequest.open("GET", mUrl, true)
+	mRequest.send();
+}
+
+
+
+
+
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
+	return function (e) {
 		galleryImage.img = e.target;
 		mImages.push(galleryImage);
 	}
 }
 
-$(document).ready( function() {
-	
+$(document).ready(function () {
+
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
-	
+
 });
 
-window.addEventListener('load', function() {
-	
+window.addEventListener('load', function () {
+
 	console.log('window loaded');
 
 }, false);
 
+//Assigning data from JSON list to variables that will be used in our slideshow 
 function GalleryImage() {
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo
 	//3. the date when the photo was taken
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
-	var location = "";
-	var description = "";
-	var date = "";
-	var img = "";
+	var location;
+	var description;
+	var date;
+	var img;
 }
 
